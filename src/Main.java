029f7edb28865ea7571dc13a7c5e13686e6072f8
fzlaziz <84029684+fzlaziz@users.noreply.github.com>
2024-05-04 00:55:24 +0700

    Fix Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         FormSupplier viewFrame = new FormSupplier();
